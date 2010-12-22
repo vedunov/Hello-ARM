f378c10da33ac6633d81abcb1506d1f447cd8fa6
vedunov <vedunov@bk.ru>
2010-12-22 17:30:43 +0300

    defs_in.mk contains permanents constants for defs.mk

---
+++ mk/defs_in.mk
@@ -0,0 +1,16 @@
+CROSS_PATH=/home/alex/sam/arm-2007q1/bin
+CROSS_PREFIX=arm-none-linux-gnueabi-
+CROSS_ENV=$(CROSS_PATH)/$(CROSS_PREFIX)
+
+CC=$(CROSS_ENV)gcc
+LD=$(CROSS_ENV)ld
+AS=$(CROSS_ENV)as
+OBJCOPY=$(CROSS_ENV)objcopy
+OBJDUMP=$(CROSS_ENV)objdump
+
+.S.o:
+	$(AS) $< -o $@
+.c.o:
+	$(CC) $(CPPFLAGS) $(CFLAGS) -o $@ -c $<
+
+
